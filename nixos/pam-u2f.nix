f12f7bfbca1c7dddb724c1bca3407bc7a335ae97
deltxprt <vincent.genest@protonmail.com>
2025-01-18 13:27:51 -0500

    added my info

--- nixos/pam-u2f.nix
+++ nixos/pam-u2f.nix
@@ -6,7 +6,7 @@
 }:
 
 let
-#  mapping = "cole:ffKBkSoiV7KcwUcdpjagrP9P4Gj4SFLxnZHRp3gy/jZbOD3J5xGCyidt9ruyA9ZT+DkV/lKd78Wy4RAEM8qodQ==,DfllypvIlTkhzZKEdVdRQb7iYev7K0TvpzaREkT2CFlhz4j/3vwWHzGBpQToxA2YOR1/Mwbm0cb4VxDJXs4k9g==,es256,+presence";
+  mapping = "delta:3mQY9hoHlogwsTRFStVuUB3vaRaNSp5+Ms970cpqHrKZGBlm0IwKbiXVZzlM0WkjS4pttMAcsgf8mM7YDJIA/w==,ZEQD+jq1zxeUZUfPfWDp4PfEAjuOrXWiRdXt27y4zV/JyJ1CYGoR6+sxkR+oXY7VG8oKZi8KVeR/uZkTc+grfw==,es256,+presence";
 
   ykDisconnect = pkgs.writeShellScript "yk-disconnect.sh" ''
     ${pkgs.procps}/bin/pkill -USR1 swayidle
