9ba57f6cb98f3d39a78f4ba6d3a52606742b79a1
Vincent Genest <vincent.genest@protonmail.com>
2025-11-23 14:31:48 -0500

    test without

--- nixos/configuration-laptop.nix
+++ nixos/configuration-laptop.nix
@@ -197,7 +197,7 @@
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
-    enable = mkForce false;
+    enable = true;
     settings = {
       # Opinionated: forbid root login through SSH.
       PermitRootLogin = "no";
