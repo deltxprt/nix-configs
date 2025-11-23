ec27795aacc8adc9ca4639b50ef56391a0591f69
Vincent Genest <vincent.genest@protonmail.com>
2025-11-23 11:32:16 -0500

    fix

--- nixos/configuration-laptop.nix
+++ nixos/configuration-laptop.nix
@@ -154,7 +154,7 @@
      playerctl
   ];
 
-  fonts.packages = with pkgs [ nerdfonts ];
+  fonts.packages = with pkgs; [ nerdfonts ];
 
   networking.hostName = "D37T4-L4PT0P";
   networking.networkmanager.enable = true;
