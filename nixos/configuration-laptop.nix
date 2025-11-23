3f7b6655bb5ed600a7d5d77bed9169ceeda9b71c
Vincent Genest <vincent.genest@protonmail.com>
2025-11-23 14:23:29 -0500

    remove quote

--- nixos/configuration-laptop.nix
+++ nixos/configuration-laptop.nix
@@ -178,7 +178,7 @@
       initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
       # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
-      extraGroups = ["wheel" "video", "networkmanager"];
+      extraGroups = ["wheel" "video" "networkmanager"];
       shell = pkgs.zsh;
     };
   };
