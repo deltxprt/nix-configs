077b4ffeb1bb7bad81938ecce1f7fce6990479b1
Vincent Genest <vincent.genest@protonmail.com>
2025-11-23 15:18:35 -0500

    no grub use hyde

--- nixos/configuration-laptop.nix
+++ nixos/configuration-laptop.nix
@@ -20,24 +20,24 @@
   boot.loader.timeout = 10;
 
   #Enable GRUB
-  boot.loader.grub = {
-    enable = true;
-    device = "nodev";
-    efiSupport = true;
-    useOSProber = true;
-    default = 0;
-    theme = pkgs.stdenv.mkDerivation {
-      pname = "distro-grub-themes";
-      version = "3.1";
-      src = pkgs.fetchFromGitHub {
-        owner = "AdisonCavani";
-        repo = "distro-grub-themes";
-        rev = "v3.1";
-        hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
-      };
-      installPhase = "cp -r customize/nixos $out";
-    };
-  };
+#  boot.loader.grub = {
+#    enable = true;
+#    device = "nodev";
+#    efiSupport = true;
+#    useOSProber = true;
+#    default = 0;
+#    theme = pkgs.stdenv.mkDerivation {
+#      pname = "distro-grub-themes";
+#      version = "3.1";
+#      src = pkgs.fetchFromGitHub {
+#        owner = "AdisonCavani";
+#        repo = "distro-grub-themes";
+#        rev = "v3.1";
+#        hash = "sha256-ZcoGbbOMDDwjLhsvs77C7G7vINQnprdfI37a9ccrmPs=";
+#      };
+#      installPhase = "cp -r customize/nixos $out";
+#    };
+#  };
 
   # networking.hostName = "nixos"; # Define your hostname.
   # Pick only one of the below networking options.
